adbb874f45e5ea0e50729f28ed25da100332c3c7
muhkelvin <muhkelvin36@gmail.com>
2023-03-01 16:41:29 +0700

    membuat relasi table

---
+++ database/migrations/2023_03_01_093338_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->text('body');
+            $table->string('image')->nullable();
+            $table->unsignedBigInteger('categories_id');
+            $table->timestamps();
+            $table->foreign('categories_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
