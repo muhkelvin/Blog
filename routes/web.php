2662aa0429488b0590734a88ee2a9c3db194f193
muhkelvin <muhkelvin36@gmail.com>
2023-03-02 10:11:13 +0700

    all

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class , 'index']);
+
+Route::resource('dashboard', DashboardController::class);
